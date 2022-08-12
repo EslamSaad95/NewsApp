9f5ca33b5e8a195844323d935d2009e6adf9eb6b
eso202 <eslamsaad45@gmail.com>
2022-08-12 14:54:05 +0200

    add news dto

--- app/src/main/java/com/example/footballLeague/data/network/ApiService.kt
+++ app/src/main/java/com/example/footballLeague/data/network/ApiService.kt
@@ -1,3 +1,13 @@
 package com.example.news.data.network
 
+import com.example.footballLeague.data.network.dto.NewsDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+
 interface ApiService
+{
+    @GET("top-headlines")
+    suspend fun getEgyptNews(@QueryMap queryMap:HashMap<String,String>):Response<NewsDto>
+}
