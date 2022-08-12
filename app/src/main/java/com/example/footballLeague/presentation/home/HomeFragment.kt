ad436252c46589cd3893bfeeb96392463c281c81
eso202 <eslamsaad45@gmail.com>
2022-08-12 14:08:07 +0200

    add items to menu_bottom_nav

---
+++ app/src/main/java/com/example/footballLeague/presentation/home/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.example.footballLeague.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.news.R
+
+class HomeFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/example/footballLeague/presentation/wishlist/WishlistFragment.kt
@@ -0,0 +1,13 @@
+package com.example.footballLeague.presentation.wishlist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.news.R
+
+
+class WishlistFragment : Fragment() {
+
+}
