7e8a8900c63d45035a568524a86b7950ab53ad61
eso202 <eslamsaad45@gmail.com>
2022-08-13 16:17:26 +0200

    finish news details

--- app/src/main/java/com/example/news/presentation/MainActivity.kt
+++ app/src/main/java/com/example/news/presentation/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.news.presentation
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -30,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         initNavigation()
         setOnDestinationChangedListener()
+        setToolbar()
     }
 
     private fun initNavigation() {
@@ -40,8 +46,37 @@ class MainActivity : AppCompatActivity() {
 
     private fun setOnDestinationChangedListener() {
         navController.addOnDestinationChangedListener { _, destination, arguments ->
+            setBackBtn(destination.id)
             arguments?.let {
                 if (it.getBoolean("showToolbar")) showToolbar() else hideToolbar()
+            }
+        }
+    }
+
+    private fun setToolbar()
+    {
+
+        addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_toolbar, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return false
+            }
+        })
+
+
+    }
+    private fun setBackBtn(id:Int)
+    {
+        val isTopLevelDestination = appBarConfiguration.topLevelDestinations.contains(id)
+        if (isTopLevelDestination.not()) {
+            binding.toolbar.apply {
+                setNavigationIcon(R.drawable.ic_back)
+                setNavigationOnClickListener {
+                    onBackPressedDispatcher.onBackPressed()
+                }
             }
         }
     }
--- app/src/main/java/com/example/news/presentation/newsDetails/NewsDetailsFragment.kt
+++ app/src/main/java/com/example/news/presentation/newsDetails/NewsDetailsFragment.kt
@@ -1,12 +1,15 @@
 package com.example.news.presentation.newsDetails
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
 import com.example.news.databinding.FragmentNewsDetailsBinding
 import com.example.news.domain.entity.TopHeadlinesEntity
+import com.example.news.R
 
 
 class NewsDetailsFragment : Fragment() {
@@ -28,6 +31,29 @@ class NewsDetailsFragment : Fragment() {
         arguments?.getParcelable<TopHeadlinesEntity>("detailsObj")?.let { detailsObj ->
             binding.news = detailsObj
         }
+        setToolbarMenuAction()
+
+    }
+
+    private fun setToolbarMenuAction()
+    {
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.itemFav -> {
+                        findNavController().navigate(R.id.actionNewsDetailsToWishlist)
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
     }
 
