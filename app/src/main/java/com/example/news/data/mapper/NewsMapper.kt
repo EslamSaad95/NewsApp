f35422d79df46425e187be8c2a9ae4e3c198b48a
eso202 <eslamsaad45@gmail.com>
2022-08-13 14:32:22 +0200

    add description to EgyptNewsEntity

--- app/src/main/java/com/example/news/data/mapper/NewsMapper.kt
+++ app/src/main/java/com/example/news/data/mapper/NewsMapper.kt
@@ -5,7 +5,14 @@ import com.example.news.domain.entity.EgyptNewsEntity
 import com.example.news.domain.entity.LatestNewsEntity
 
 fun List<NewsDto.Article>.toEgyptNewsEntity(): List<EgyptNewsEntity> {
-    return this.map { EgyptNewsEntity(it.urlToImage,it.title, date = it.publishedAt) }
+    return this.map {
+        EgyptNewsEntity(
+            it.urlToImage,
+            it.title,
+            date = it.publishedAt,
+            description = it.description
+        )
+    }
 }
 
 fun List<NewsDto.Article>.toLatestNews(): List<LatestNewsEntity> {
