6c1099a43fcf4478ddde5506d1475a30b71f189a
eso202 <eslamsaad45@gmail.com>
2022-08-13 19:42:38 +0200

    finish search results adapter

---
+++ app/src/main/java/com/example/news/presentation/search/SearchResultsRvAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.news.presentation.search
+
+import com.example.news.databinding.ItemNewsBinding
+import com.example.news.domain.entity.TopHeadlinesEntity
+import com.example.news.presentation.utils.base.BaseAdapter
+
+class SearchResultsRvAdapter : BaseAdapter<ItemNewsBinding, TopHeadlinesEntity>() {
+    override fun setContent(binding: ItemNewsBinding, item: TopHeadlinesEntity, position: Int) {
+        binding.apply {
+            news = item
+            root.setOnClickListener { onViewClicked(it, item, position) }
+        }
+    }
+}
